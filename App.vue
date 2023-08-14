9a6d44b433cf665d5633d6196dac1f1f689f063a
raqib907 <106022396+raqib907@users.noreply.github.com>
2023-08-14 21:19:16 +0600

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,54 @@
+
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import PieChart from "./components/PieChart.vue";
+
+const slides = [
+  'image1.jpg',
+  'image2.jpg',
+  'image3.jpg',
+  // Add more image URLs here
+];
+
+const currentSlide = ref(0);
+
+const prevSlide = () => {
+  currentSlide.value = (currentSlide.value - 1 + slides.length) % slides.length;
+};
+
+const nextSlide = () => {
+  currentSlide.value = (currentSlide.value + 1) % slides.length;
+};
+
+const interval = setInterval(nextSlide, 5000); // Auto-advance every 5 seconds
+
+onMounted(() => {
+  // Initialization logic here
+});
+
+onBeforeUnmount(() => {
+  clearInterval(interval);
+});
+</script>
+
+
+<template>
+  <div class="carousel">
+    <button @click="prevSlide">Previous</button>
+    <div class="slide-container">
+      <transition name="fade" mode="out-in">
+        <img :key="currentSlide" :src="slides[currentSlide]" alt="Carousel Slide" />
+      </transition>
+    </div>
+    <button @click="nextSlide">Next</button>
+  </div>
+
+  <PieChart></PieChart>
+</template>
+
+
+
+<style>
+/* Add your styles for the carousel here */
+</style>
---
+++ components/PieChart.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="pie-chart">
+    <canvas ref="chartCanvas"></canvas>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import { Pie } from 'vue-chartjs';
+
+const chartCanvas = ref(null);
+
+onMounted(() => {
+  // Fetch data or manually create data for the pie chart
+  const chartData = {
+    labels: ['Red', 'Blue', 'Yellow'],
+    datasets: [
+      {
+        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+        data: [300, 50, 100],
+      },
+    ],
+  };
+
+  const chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+  };
+
+  // Create the pie chart
+  new Pie(chartCanvas.value, {
+    data: chartData,
+    options: chartOptions,
+  });
+});
+
+onBeforeUnmount(() => {
+  // Clean up any ongoing tasks related to the chart
+  // For example, you might need to destroy the chart instance
+});
+</script>
+
+<style>
+/* Add your styles for the pie chart container here */
+</style>
